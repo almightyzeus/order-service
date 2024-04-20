52dcc57110b3298fccf6360b5863376c19835e4f
almightyzeus <chinmaypgoa@gmail.com>
2024-04-20 22:53:26 +0530

    Change user input to match with the user model

--- orderModel.js
+++ orderModel.js
@@ -23,7 +23,7 @@ productSchema.set('toJSON', { virtuals: true });
 
 const orderSchema = new mongoose.Schema({
   userId: {
-    type: mongoose.Schema.Types.ObjectId,
+    type: Number,
     required: true
   },
   productList: [productSchema], // Array of product items
